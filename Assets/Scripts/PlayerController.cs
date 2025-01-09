0117623c51ff3639fd3ccd7df0ac93b363dcf8f1
Mike McGhen <mcghenmike@gmail.com>
2025-01-08 23:24:07 -0500

    feat: Adds ground check for proper jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxSpeed = 2f;
-    public float jump;
-    Transform self;
-    Rigidbody2D rb;
+    public float maxSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
     void Start()
     {
-        self = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && (isGrounded || Mathf.Abs(rb.linearVelocity.y) < 0.1f))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate()
     {
-        var horizontalMagnitude = Input.GetAxis("Horizontal");
-
-        rb.MovePosition(new Vector2(self.position.x, self.position.y) + (new Vector2(horizontalMagnitude, 0) * maxSpeed));
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * maxSpeed, rb.linearVelocity.y);
     }
 }
