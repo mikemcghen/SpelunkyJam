e48af7486f3fac2f28fce53b562e6f0128359084
Mike McGhen <mcghenmike@gmail.com>
2025-01-11 12:43:14 -0500

    Revert "Merge branch 'pete/tilemap-exp' of https://github.com/mikemcghen/SpelunkyJam into pete/tilemap-exp"
    This reverts commit a0e9789bd362ddc34df26c2ccc0f2cb3b08dfe92, reversing
    changes made to 74048ddd82d5dd49f1b3af903a1294cd58ebdf98.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,33 +2,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxSpeed = 5f;
-    public float jumpForce = 5f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public float groundCheckRadius = 0.2f;
-
-    private Rigidbody2D rb;
-    private bool isGrounded;
+    public float maxSpeed = 2f;
+    Transform self;
+    Rigidbody2D rb;
 
     void Start()
     {
+        self = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
     }
 
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(horizontalInput * maxSpeed, rb.linearVelocity.y);
+        var horizontalMagnitude = Input.GetAxis("Horizontal");
+
+        rb.MovePosition(new Vector2(self.position.x, self.position.y) + (new Vector2(horizontalMagnitude, 0) * maxSpeed));
     }
 }
