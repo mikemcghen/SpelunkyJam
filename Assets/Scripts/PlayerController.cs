a5e6184b78078ac4beb6675e6d13e958c8df0bae
Mike McGhen <mcghenmike@gmail.com>
2025-01-14 23:37:43 -0500

    feat: Reworks Player controller for better jump and fleshed out ladder mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,19 @@
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {
     public float maxSpeed = 5f;
     public float jumpForce = 5f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public float groundCheckRadius = 0.2f;
+    public float regrabDistance = 0.5f;
 
     private Rigidbody2D rb;
     private bool isGrounded;
-    private float vertical;
-    private float ladderCenterX;
     private bool isLadder;
     private bool isClimbing;
-
+    private float vertical;
+    private float ladderCenterX;
+    private float ladderTopY;
+    private bool canRegrabLadder = true;
 
     void Start()
     {
@@ -24,36 +22,54 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        //this works. need to handle breaking away from ladder center when jumping
-        if (Input.GetButtonDown("Jump") && (isGrounded || isClimbing))
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (isGrounded || isClimbing)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                if (isClimbing)
+                {
+                    isClimbing = false;
+                    canRegrabLadder = false;
+                }
+            }
         }
 
         vertical = Input.GetAxis("Vertical");
 
-        if (isLadder && Mathf.Abs(vertical) > 0f)
+        if (isLadder && Mathf.Abs(vertical) > 0f && canRegrabLadder && Mathf.Abs(transform.position.x - ladderCenterX) <= regrabDistance)
         {
-            isClimbing = true;
+            if (vertical > 0f && transform.position.y >= ladderTopY)
+            {
+                vertical = 0f;
+            }
+            else
+            {
+                isClimbing = true;
+            }
+        }
+
+        if (rb.linearVelocity.y < 0f)
+        {
+            canRegrabLadder = true;
         }
     }
 
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(horizontalInput * maxSpeed, rb.linearVelocity.y);
 
         if (isClimbing)
         {
             rb.gravityScale = 0f;
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * maxSpeed);
-            rb.transform.position = new Vector2(ladderCenterX, rb.transform.position.y);
+            rb.linearVelocity = new Vector2(0f, vertical * maxSpeed);
+
+            rb.position = new Vector2(ladderCenterX, rb.position.y);
         }
         else
         {
             rb.gravityScale = 1f;
+            rb.linearVelocity = new Vector2(horizontalInput * maxSpeed, rb.linearVelocity.y);
         }
     }
 
@@ -63,6 +79,9 @@ public class PlayerController : MonoBehaviour
         {
             isLadder = true;
             ladderCenterX = collision.gameObject.transform.position.x;
+
+            Collider2D ladderCollider = collision.GetComponent<Collider2D>();
+            ladderTopY = ladderCollider.bounds.max.y;
         }
     }
 
@@ -73,5 +92,15 @@ public class PlayerController : MonoBehaviour
             isLadder = false;
             isClimbing = false;
         }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
